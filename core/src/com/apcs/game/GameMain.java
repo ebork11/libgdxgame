0f9507b08b0028f644e9f8ebe6cdb58844310bc3
Ethan Bork <ebork11@gmail.com>
2019-04-23 14:05:13 -0700

    Added player movement as well as cleaned some things up

--- core/src/com/apcs/game/GameMain.java
+++ core/src/com/apcs/game/GameMain.java
@@ -3,35 +3,40 @@ package com.apcs.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameMain extends ApplicationAdapter {
 
-	PlayerHandler player;
+	private PlayerHandler player;
 
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("core/assets/badlogic.jpg");
 		player = new PlayerHandler();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(.1f, .1f, .1f, 1); // sets the basic background color
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(player.getTexture(), player.getCollider().x, player.getCollider().y);
-		batch.end();
+
+		playerManage(); // handles everything regarding the player
+
+		batch.begin(); // beginning of where everything is drawn
+		batch.draw(player.getTexture(), player.getCollider().x, player.getCollider().y); // draws the player at the colliders location
+		batch.end(); // ending of where everything is drawn
+	}
+
+
+	public void playerManage() {
+		player.movementHandler(); // checks the keyboard for input and moves the player accordingly
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		player.disposer(); // disposes all of the players textures
 	}
 }
--- core/src/com/apcs/game/PlayerHandler.java
+++ core/src/com/apcs/game/PlayerHandler.java
@@ -1,5 +1,7 @@
 package com.apcs.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 
@@ -12,19 +14,48 @@ public class PlayerHandler {
         collider = new Rectangle(0, 0, myTexture.getWidth(), myTexture.getHeight());
     }
 
+    /*
+        Checks for input on WASD keys and moves the player according to what they press.
+     */
+    public void movementHandler() {
+        float speed = 5f; // speed to move the player
 
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) { // if the w key is pressed
+            collider.y += speed; // move the collider up
+        } else if (Gdx.input.isKeyPressed(Input.Keys.S)) { // if the s key is pressed
+            collider.y -= speed; // move the collider down
+        } else if (Gdx.input.isKeyPressed(Input.Keys.A)) { // if the a key is pressed
+            collider.x -= speed; // move the collider left
+        } else if (Gdx.input.isKeyPressed(Input.Keys.D)) { // if the d key is pressed
+            collider.x += speed; // move the collider right
+        }
+    }
+
+    /*
+        Returns the players texture
+     */
     public Texture getTexture() {
         return myTexture;
     }
 
+    /*
+        Sets the players texture to whatever is given
+     */
     public void setTexture(Texture newTex) {
         myTexture = newTex;
     }
 
-
+    /*
+        Returns the players collider object(aka the Rectangle)
+     */
     public Rectangle getCollider() {
         return collider;
     }
+
+    public void disposer() {
+        myTexture.dispose();
+    }
+
 
 
 
