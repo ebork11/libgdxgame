65d84053ec966a6f74b4d117658a82452c276a4f
Ethan <ebork11@gmail.com>
2019-05-02 19:39:15 -0700

    Fixed some minor errors

--- core/src/com/apcs/game/rooms/DefaultRoom.java
+++ core/src/com/apcs/game/rooms/DefaultRoom.java
@@ -9,22 +9,14 @@ import java.util.ArrayList;
 public class DefaultRoom extends Room {
     private Texture myFloor;
     private ArrayList<Door> doors;
-    private static ArrayList<Entity> entities;
+    private ArrayList<Entity> entities;
 
     public DefaultRoom() {
         myFloor = new Texture("core/assets/rooms/background2.png"); // sets texture
         doors = new ArrayList<Door>();
         entities = new ArrayList<Entity>();
-    }
-
-    public DefaultRoom(Room pastRoom, String loc) {
-        myFloor = new Texture("core/assets/rooms/background2.png"); // sets texture
-        doors = new ArrayList<Door>();
-        entities = new ArrayList<Entity>();
 
         entities.add(new Entity());
-
-        doors.add(new Door(pastRoom, loc));
     }
 
     public Texture getFloor() {
