a70199bf23fa4fb692c8695f3fc5d6f119273b7b
Ethan <ebork11@gmail.com>
2019-05-03 14:20:06 -0700

    Finished charger enemy

--- core/src/com/apcs/game/enemies/Charger.java
+++ core/src/com/apcs/game/enemies/Charger.java
@@ -4,32 +4,38 @@ import com.apcs.game.player.PlayerHandler;
 
 public class Charger extends Entity {
 
-    private int moveCooldown;
+    private int moveCooldown, chargeTime;
     private float moveX, moveY;
+    private long chargeCooldown, start;
 
     public Charger(){
         super();
         setHealth(10);
-        setCharcater("core/assets/player.png");
-        setSpeed(8f);
+        setCharcater("core/assets/clark.png");
+        setSpeed(15f);
         setStrength(1);
-        setCooldown(500);
         moveCooldown = 2000;
+        chargeTime = 1000;
+        chargeCooldown = System.currentTimeMillis();
     }
 
     public void move() {
-        if (System.currentTimeMillis() - getIncoll() > moveCooldown) {
-           // setInColl(System.currentTimeMillis());
-            walk();
+
+        if (System.currentTimeMillis() - chargeCooldown > moveCooldown) {
+            walk(moveX, moveY);
+
+            if (System.currentTimeMillis() - start > chargeTime) {
+                chargeCooldown = System.currentTimeMillis();
+            }
         } else {
-            moveX = PlayerHandler.getCollider().x;
-            moveY = PlayerHandler.getCollider().y;
+            moveX = PlayerHandler.getCollider().x - getCollider().x;
+            moveY = PlayerHandler.getCollider().y - getCollider().y;
+
+            start = System.currentTimeMillis();
         }
     }
 
-    public void walk(){
-        float x = moveX;
-        float y = moveY;
+    public void walk(float x, float y){
         double ang1 = Math.atan(y / x);
         double ang2 = Math.atan(x / y);
         float sx = (float)(getSpeed()*(Math.sin(ang2)));
@@ -39,17 +45,29 @@ public class Charger extends Entity {
         sy = Math.abs(sy);
 
         if (x > 0 && y > 0) {
-            getCollider().x += sx;
-            getCollider().y += sy;
+            if (getCollider().x + getTexture().getWidth() < 920) {
+                getCollider().x += sx;
+            } if (getCollider().y < 650) {
+                getCollider().y += sy;
+            }
         } else if (x < 0 && y > 0) {
-            getCollider().x -= sx;
-            getCollider().y += sy;
+            if (getCollider().x > 40) {
+                getCollider().x -= sx;
+            } if (getCollider().y < 650) {
+                getCollider().y += sy;
+            }
         }  else if (x < 0 && y < 0) {
-            getCollider().x -= sx;
-            getCollider().y -= sy;
+            if (getCollider().x > 40) {
+                getCollider().x -= sx;
+            } if (getCollider().y > 40) {
+                getCollider().y -= sy;
+            }
         }  else if (x > 0 && y < 0) {
-            getCollider().x += sx;
-            getCollider().y -= sy;
+            if (getCollider().x + getTexture().getWidth() < 920) {
+                getCollider().x += sx;
+            } if (getCollider().y > 40) {
+                getCollider().y -= sy;
+            }
         }
     }
 
