596df8623bbdc260a410bfd0850842c79170ae6c
Ethan <ebork11@gmail.com>
2019-05-30 16:45:37 -0700

    added epic music

--- core/src/com/apcs/game/GameMain.java
+++ core/src/com/apcs/game/GameMain.java
@@ -18,6 +18,7 @@ import com.apcs.game.rooms.RoomManager;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -55,10 +56,16 @@ public class GameMain extends ApplicationAdapter {
 	private long cooldown; // spike cooldown??? needs to be redone
 	public static long enteredNewRoom;
 
+	Music music;
+
 	@Override
 	public void create () {
 
 		batch = new SpriteBatch();
+
+		music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+		music.setVolume(0.4f);
+		music.setLooping(true);
 
 		font = new BitmapFont();
 		font.setColor(Color.WHITE);
@@ -91,6 +98,8 @@ public class GameMain extends ApplicationAdapter {
 		formulas, and handle any other changes such as movement and/or combat
 	 */
 	public void render () {
+		music.play();
+
 		Gdx.gl.glClearColor(.1f, .1f, .1f, 1); // sets the basic background color
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
